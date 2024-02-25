85b3b3ba6e6c3ee8e9e3979a531dace789b95de0
ml <ml@gmail.com>
2024-02-25 13:29:51 +0100

    c1master

---
+++ git.java
@@ -0,0 +1,12 @@
+creer dossier
+deplacer entre le dossier
+creer des fichires par exemple ex.java
+creer banch avec git checkout -b <nom_de_la_branche>
+git init
+git add .
+git status
+git config --global user.email "exemple@gmail.com"
+git config --global user.email "exemple@gmail.com"
+git commit -m "premier commit"
+git remote add origin https://github.com/MDLAGZOULI/repo.git
+git push -u origin master
